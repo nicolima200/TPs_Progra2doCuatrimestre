2933a7c69c4d65a6ebc0d31350198d315ef70ff4
nicolima200 <nicolima200@gmail.com>
2025-09-01 20:53:04 -0300

    Agregado PDF con capturas

--- TP 3/Estudiantes/src/tpclases/Estudiante.java
+++ TP 3/Estudiantes/src/tpclases/Estudiante.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tpclases;
-
-/**
- *
- * @author nicol
- */
 public class Estudiante {
     String nombre;
     String apellido;
